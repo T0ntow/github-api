5dbb3aa90a5294d8cedb592ab9105ef92a064d25
Wellington Batista <wellingtonbs109@gmail.com>
2023-07-13 21:10:49 -0300

    feat: added main functions

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { format } from 'date-fns';
 
 @Component({
   selector: 'app-home',
@@ -9,26 +10,33 @@ export class HomePage {
 
   projects: any[] = [];
   searchName: string = '';
+  notFound!: boolean ;
 
   constructor() {
-    this.searchUser('Flipesouza16');
   }
 
+  formatDate(date: string): string {
+    const formattedDate = format(new Date(date), 'dd/MM/yyyy');
+    return formattedDate;
+  }
 
-  async searchUser(user: string) {
+  async searchUser() {
+    this.projects = [];
+    const APIResponse = await fetch(`https://api.github.com/users/${this.searchName}/repos`);
 
-    const APIResponse = await fetch(`https://api.github.com/${user}/repos`)
+    console.log(APIResponse);
+
+
     if (APIResponse.status === 200) {
       const data = await APIResponse.json();
-
       this.getProjects(data);
-      console.log(data);
+      this.notFound = false;
 
-      return data;
-
+    } else if (APIResponse.status === 404) {
+      this.notFound = true;
     } else {
       console.log("BO");
-
+      
     }
   }
 
@@ -40,9 +48,18 @@ export class HomePage {
     console.log(this.projects);
   }
 
-  async onSearchChange() {
-    await this.searchUser(this.searchName);
+  onSearchChange() {
+    this.searchUser();
     console.log("this.searchName", this.searchName);
+  }
+
+  openProject(project: any) {
+    const fullName = project.full_name;
+    const url = `https://github.com/${fullName}`;
+
+    console.log(url);
+    
+    window.open(url, '_blank');
   }
 
 }
